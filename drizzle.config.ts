333c9044d7d5ca5026ae53e04518674cb18dbefc
Kodie <kodii.tulip@gmail.com>
2025-03-27 15:31:12 -0300

    feat: drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import type { Config } from 'drizzle-kit';
+
+config({ path: '.env.local' });
+
+const drizzleConfig: Config = {
+  schema: './db/schema.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+};
+
+export default drizzleConfig;
