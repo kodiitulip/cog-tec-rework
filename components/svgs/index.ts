3b5a772533e78f077ff0e7223522f6a5274b9c38
Kodie <kodii.tulip@gmail.com>
2025-04-24 10:56:02 -0300

    feat: added react components of realm icons

--- components/svgs/index.ts
+++ components/svgs/index.ts
@@ -1,5 +1,8 @@
+import BehaviorismIcon from './behaviorism/icon';
 import CogTecIcon from './cog-tec/icon';
+import GestaltIcon from './gestalt/icon';
+import SociocultureIcon from './socioculture/icon';
 
 export type IconProps = React.ComponentProps<'svg'> & {};
 
-export { CogTecIcon };
+export { CogTecIcon, BehaviorismIcon, GestaltIcon, SociocultureIcon };
