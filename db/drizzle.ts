74861b358899fa9c9dc19c08b6e2328443623d75
kodiitulip <kodii.tulip@gmail.com>
2025-04-29 17:42:59 -0300

    feat: local supabase

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,10 +1,33 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 import * as schema from './schema';
+import { createClient } from '@/lib/supabase/server';
+import { createDrizzle } from './db-client';
+import { decode } from './jwt';
+import { DrizzleConfig } from 'drizzle-orm';
 
-config({ path: '.env.local' });
+const config = {
+  casing: 'snake_case',
+  schema,
+} satisfies DrizzleConfig<typeof schema>;
 
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
-export const db = drizzle({ client, schema });
+// ByPass RLS
+const admin = drizzle({
+  client: postgres(process.env.ADMIN_DATABASE_URL!, { prepare: false }),
+  ...config,
+});
+// Protected by RLS
+const client = drizzle({
+  client: postgres(process.env.DATABASE_URL!, { prepare: false }),
+  ...config,
+});
+
+// export const db = drizzle({ client, schema });
+
+export async function createDrizzleSupabaseClient() {
+  const {
+    data: { session },
+  } = await (await createClient()).auth.getSession();
+  return createDrizzle(decode(session?.access_token ?? ''), { admin, client });
+}
--- db/queries.ts
+++ db/queries.ts
@@ -1,24 +1,26 @@
 import { cache } from 'react';
-import { db } from '@/db/drizzle';
+import { createDrizzleSupabaseClient } from '@/db/drizzle';
 import { createClient } from '@/lib/supabase/server';
 import { eq } from 'drizzle-orm';
-import { units, userProgress, courses,        challengeProgress } from '@/db/schema';
+import { units, userProgress, courses, challengeProgress } from '@/db/schema';
 
 export const getCourses = cache(async () => {
-  const data = await db.query.courses.findMany({
+  const db = await createDrizzleSupabaseClient();
+  const data = await db.admin.query.courses.findMany({
     orderBy: ({ id }, { asc }) => [asc(id)],
   });
   return data;
 });
 
 export const getUserProgress = cache(async () => {
+  const db = await createDrizzleSupabaseClient();
   const { auth } = await createClient();
   const {
     data: { user },
   } = await auth.getUser();
   if (!user) return undefined;
 
-  const data = await db.query.userProgress.findFirst({
+  const data = await db.admin.query.userProgress.findFirst({
     where: eq(userProgress.userId, user.id),
     with: {
       activeCourse: true,
@@ -29,47 +31,51 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+  const db = await createDrizzleSupabaseClient();
   const userProgress = await getUserProgress();
 
   if (!userProgress || !userProgress.userId || !userProgress.activeCourseId) {
     return [];
   }
 
-  const data = await db.query.units.findMany({
+  const data = await db.admin.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
       lessons: {
-    orderBy: ({ order }, { asc }) => [asc(order)],
+        orderBy: ({ order }, { asc }) => [asc(order)],
         with: {
           challenges: {
             with: {
               challengeProgress: {
                 where: eq(challengeProgress.userId, userProgress.userId),
               },
-            }
-          }
-        }
-      }
-    }
-  })
+            },
+          },
+        },
+      },
+    },
+  });
 
   const normalizedData = data.map((unit) => {
     const completedLessons = unit.lessons.map((lesson) => {
-      const completed = lesson.challenges.every(({challengeProgress}) => {
-        return challengeProgress && challengeProgress.length > 0 && challengeProgress.every(({completed}) => completed)
-      })
-      return {...lesson, completed }
-    })
-    return {...unit, lessons: completedLessons}
-  })
+      const completed = lesson.challenges.every(({ challengeProgress }) => {
+        return (
+          challengeProgress && challengeProgress.length > 0 && challengeProgress.every(({ completed }) => completed)
+        );
+      });
+      return { ...lesson, completed };
+    });
+    return { ...unit, lessons: completedLessons };
+  });
 
-  return normalizedData
-})
+  return normalizedData;
+});
 
 export const getCourseById = cache(async (courseId: number) => {
-  const data = await db.query.courses.findFirst({
+  const db = await createDrizzleSupabaseClient();
+  const data = await db.admin.query.courses.findFirst({
     where: eq(courses.id, courseId),
     // TODO: Populate units and lessons
-  })
+  });
   return data;
-})
+});
