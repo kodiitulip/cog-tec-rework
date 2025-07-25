68ca6eb7b01008b61d8e1426dcac1a90931ae832
kodiitulip <kodii.tulip@gmail.com>
2025-07-25 16:15:30 -0300

    feat: using suspense on the layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 import { MobileHeader } from '@/components/bits/mobile-header';
 import { Sidebar } from '@/components/bits/sidebar';
+import { LoadingIcon } from '@/components/misc/loading';
+import { Suspense } from 'react';
 
 type Props = {
   children: React.ReactNode;
@@ -10,7 +12,9 @@ const MainLayout = ({ children }: Readonly<Props>) => (
     <MobileHeader />
     <Sidebar className='hidden lg:flex bg-ecstasy-100' />
     <main className='lg:pl-64 h-full pt-12.5 lg:pt-0 overflow-y-auto'>
-      <div className='max-w-264 mx-auto h-full pt-6'>{children}</div>
+      <Suspense fallback={<LoadingIcon />}>
+        <div className='max-w-264 mx-auto h-full pt-6'>{children}</div>
+      </Suspense>
     </main>
   </>
 );
