2ccbed1a5be12406d294ab77c9a0cf120b9af0de
kodiitulip <kodii.tulip@gmail.com>
2025-07-31 14:26:54 -0300

    feat: added loading.tsx to all pages

---
+++ app/(main)/leaderboard/loading.tsx
@@ -0,0 +1,11 @@
+import { LoadingIcon } from '@/components/misc/loading';
+
+const Loading = () => {
+  return (
+    <div className='h-full w-full flex items-center justify-center'>
+      <LoadingIcon className='size-6' />
+    </div>
+  );
+};
+
+export default Loading;
