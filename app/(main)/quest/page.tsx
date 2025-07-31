f2152085ae32ce0b67e9eb0ad9e6940453d78124
kodiitulip <kodii.tulip@gmail.com>
2025-07-31 13:40:34 -0300

    feat: quest page

--- app/(main)/quest/page.tsx
+++ app/(main)/quest/page.tsx
@@ -1,9 +1,29 @@
 import { FeedWrapper } from '@/components/bits/pages/feed-wrapper';
 import { StickyWrapper } from '@/components/bits/pages/sticky-wrapper';
 import { UserProgress } from '@/components/bits/pages/user-progress';
+import { Progress } from '@/components/ui/progress';
 import { getUserProgress } from '@/db/queries';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+
+const tempquests = [
+  {
+    title: 'Ganhe 20 XP',
+    value: 20,
+  },
+  {
+    title: 'Ganhe 50 XP',
+    value: 50,
+  },
+  {
+    title: 'Ganhe 100 XP',
+    value: 100,
+  },
+  {
+    title: 'Ganhe 300 XP',
+    value: 300,
+  },
+];
 
 const QuestsPage = async () => {
   const [userProgress] = await Promise.all([getUserProgress()]);
@@ -29,6 +49,19 @@ const QuestsPage = async () => {
           />
           <h1 className='text-center font-bold text-neutral-800 text-2xl my-6'>Missões</h1>
           <p className='tex-neutral-500 text-center text-lg mb-6'>Complete missões ganhando pontos</p>
+          <p className='text-rose-600/70 text-center font-bold tracking-wider uppercase'>🚧 Em construção 🚧</p>
+          <ul>
+            {tempquests.map(({title, value}) => {
+              const progress = (userProgress.points / value) * 100
+              return <div key={title} className='flex items-center w-full p-4 gap-x-4 border-t-2'>
+                <Image src='/icons/points.svg' alt='points' width={60} height={60}/>
+                <div className='flex flex-col gap-y-2 w-full'>
+                  <p className='text-neutral-700 text-xl font-bold'>{title}</p>
+                  <Progress classNameIndicator='bg-ecstasy-500' value={progress} className='h-3' />
+                </div>
+              </div>
+            })}
+          </ul>
         </div>
       </FeedWrapper>
     </div>
